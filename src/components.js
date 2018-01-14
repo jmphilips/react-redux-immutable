b7f1d2a7823f3e989ea42491db12211298357e7b
Joshua Philips <jmichaelphilips@gmail.com>
2018-01-14 10:05:06 -0600

    Added the basic todolist display.

--- src/components.js
+++ src/components.js
@@ -8,3 +8,19 @@ export function Todo(props) {
     return <span>{todo.text}</span>;
   }
 }
+
+export function TodoList(props) {
+  const { todos } = props;
+  return (
+    <div className='todo'>
+      <input type='text' placeholder='Add todo' />
+      <ul className='todo__list'>
+        {todos.map(t => (
+          <li key={t.id} className='todo__item'>
+            <Todo todo={t} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
