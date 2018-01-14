c9e3e7df39fca5d27681ce274f42d9bc5f0f2428
Joshua Philips <jmichaelphilips@gmail.com>
2018-01-14 11:04:14 -0600

    Updated the data to use immutable datas structures.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { TodoList } from './components';
+import { List, Map } from 'immutable';
 const UUID = require('uuidjs')
 
-const starterTodos = [
-  { id: UUID.generate(), isDone: true,  text: 'make components' },
-  { id: UUID.generate(), isDone: false, text: 'design actions' },
-  { id: UUID.generate(), isDone: false, text: 'implement reducer' },
-  { id: UUID.generate(), isDone: false, text: 'connect components' }
-];
+const starterTodos = List([
+  Map({ id: 0, isDone: true,  text: 'make components' }),
+  Map({ id: 1, isDone: false, text: 'design actions' }),
+  Map({ id: 2, isDone: false, text: 'implement reducer' }),
+  Map({ id: 3, isDone: false, text: 'connect components' })
+]);
 
 render(
-  <h1>Hello</h1>,
+  <TodoList todos={starterTodos} />,
   document.getElementById('app')
 );
