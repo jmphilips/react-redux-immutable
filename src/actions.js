bf2010b8b40d627b4a6314cad5fddb4ff688de69
Joshua Philips <jmichaelphilips@gmail.com>
2018-01-14 11:05:40 -0600

    Added the actions.

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+const UUID = require('uuidjs')
+
+export function addTodo(text) {
+  return {
+    type: 'ADD_TODO',
+    payload: {
+      id: UUID.generate(),
+      isDone: false,
+      text: text
+    }
+  };
+}
+
+export function toggleTodo(id) {
+  return {
+    type: 'TOGGLE_TODO',
+    payload: id
+  }
+}
