09e19b8889135074c21d35af83543656c1f2647a
Joshua Philips <jmichaelphilips@gmail.com>
2018-01-14 09:41:14 -0600

    Created the initial data structure and created some dummy data.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+const UUID = require('uuidjs')
 
+const starterTodos = [
+  { id: UUID.generate(), isDone: true,  text: 'make components' },
+  { id: UUID.generate(), isDone: false, text: 'design actions' },
+  { id: UUID.generate(), isDone: false, text: 'implement reducer' },
+  { id: UUID.generate(), isDone: false, text: 'connect components' }
+];
 
 render(
   <h1>Hello</h1>,
